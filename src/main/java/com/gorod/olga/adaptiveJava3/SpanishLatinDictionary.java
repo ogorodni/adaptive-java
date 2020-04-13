c53d7abbf87c7dd0b6a7c046f9eba066d63fc785
Olga Gorodnitskaya <o.gorodnitskaya@gmail.com>
2020-04-13 23:23:30 +0300

    Dictionary

---
+++ src/main/java/com/gorod/olga/adaptiveJava3/SpanishLatinDictionary.java
@@ -0,0 +1,87 @@
+package com.gorod.olga.adaptiveJava3;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Once, while looking through the old books in the attic, schoolboy Mike found the Spanish-Latin dictionary. By that time, his knowledge of Spanish was perfect and his dream was to learn Latin. Therefore, his discovery was very useful.
+ * <p>
+ * Unfortunately, this one dictionary is not enough to learn the language: besides the Spanish-Latin dictionary, he needs a Latin-Spanish one. In the absence of better, he decided to make the second dictionary from the first one.
+ * <p>
+ * As it is well known, any dictionary consists of translated words, each of which has a few words-translations. For each of the Latin words, which are present somewhere in the dictionary, Mike offers to find all of their translations (i.e. all the Spanish words, for which our Latin word was specified in the list of its translations), and consider only such words the translations of the particular Latin word.
+ * <p>
+ * Help Mike to complete the task on creation of the Latin-Spanish dictionary from the Spanish-Latin one.
+ * <p>
+ * Input data
+ * <p>
+ * The first line contains the only integer number N that is the number of Spanish words in the dictionary. Then N descriptions follow: each of the descriptions is located in a separate line, where first goes a Spanish word, next goes the space separated dash (symbol number 45), and then go the translations of this Spanish word into Latin, separated by spaces and commas. Translations are sorted in the lexicographic order. The order of the Spanish words in the dictionary is also lexicographic.
+ * <p>
+ * All words consist of only the lowercase Latin letters; length of each word does not exceed 15 characters. The total number of words at the input is not greater than 100000.
+ * <p>
+ * Output data
+ * <p>
+ * Output the Latin-Spanish dictionary, corresponding to the given one, strictly observing the format of the input data. In particular, the first should be the translation of a lexicographically minimal Latin word, further - the second in this order, etc. Spanish words inside the translation must also be sorted in a lexicographic order.
+ * <p>
+ * Sample Input:
+ * <p>
+ * 3
+ * apple - malum, pomum, popula
+ * fruit - baca, bacca, popum
+ * punishment - malum, multa
+ * Sample Output:
+ * <p>
+ * 7
+ * baca - fruit
+ * bacca - fruit
+ * malum - apple, punishment
+ * multa - punishment
+ * pomum - apple
+ * popula - apple
+ * popum - fruit
+ */
+
+public class SpanishLatinDictionary {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = Integer.parseInt(in.nextLine());
+        TreeMap<String, Set<String>> spanishLatinDictionary = new TreeMap<>();
+        for (int i = 0; i < num; i++) {
+            String[] record = in.nextLine().split("-");
+            String spanish = record[0].trim();
+            Set<String> latin = Arrays.stream(record[1].split(",")).map(String::trim).collect(Collectors.toSet());
+            spanishLatinDictionary.put(spanish, latin);
+        }
+
+        TreeMap<String, Set<String>> latinToSpanishDictionary = Dictionary.convert(spanishLatinDictionary);
+        System.out.println(Dictionary.toString(latinToSpanishDictionary));
+    }
+
+    public static class Dictionary {
+
+        public static TreeMap<String, Set<String>> convert(TreeMap<String, Set<String>> dict) {
+            TreeMap<String, Set<String>> result = new TreeMap<>();
+
+            for (Map.Entry<String, Set<String>> entry : dict.entrySet()) {
+                for (String word : entry.getValue()) {
+                    Set<String> translatedWords;
+                    if (result.containsKey(word)) {
+                        translatedWords = result.get(word);
+                    } else {
+                        translatedWords = new TreeSet<>();
+                    }
+                    translatedWords.add(entry.getKey());
+                    result.put(word, translatedWords);
+                }
+            }
+            return result;
+        }
+
+        public static String toString(TreeMap<String, Set<String>> dict) {
+            String result = "";
+            for (Map.Entry<String, Set<String>> entry : dict.entrySet()) {
+                result = result + entry.getKey() + " - " + entry.getValue().stream().collect(Collectors.joining(", ")) + "\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ src/test/java/com/gorod/olga/adaptiveJava3/SpanishLatinDictionaryTest.java
@@ -0,0 +1,34 @@
+package com.gorod.olga.adaptiveJava3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import static org.junit.Assert.*;
+
+public class SpanishLatinDictionaryTest {
+
+    @Test
+    public void test(){
+        TreeMap<String, Set<String>> spanishLatinDictionary = new TreeMap<>(Map.of(
+                "apple", Set.of("malum", "pomum", "popula"),
+                "fruit", Set.of("baca", "bacca", "popum"),
+                "punishment", Set.of("malum", "multa")
+        ));
+
+        TreeMap<String, Set<String>> latinSpanishDictionary = new TreeMap<>(Map.of(
+                "baca", Set.of("fruit"),
+                "bacca", Set.of("fruit"),
+                "malum", Set.of("apple", "punishment"),
+                "multa", Set.of("punishment"),
+                "pomum", Set.of("apple"),
+                "popula", Set.of("apple"),
+                "popum", Set.of("fruit")
+        ));
+        Assert.assertEquals(latinSpanishDictionary, SpanishLatinDictionary.Dictionary.convert(spanishLatinDictionary));
+    }
+
+}
